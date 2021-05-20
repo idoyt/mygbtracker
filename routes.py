557d1ec0cad4767f3fcaacbbe35485da86efe41d
kayven <18300@burnside.school.nz>
2021-05-21 10:21:06 +1200

    rename stuff, added the do query fuction, trying to get wrapper work.

--- routes.py
+++ routes.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
+db = "mygbdatabase.db"
 
+
+def do_query(query, fetch):
+    connection = sqlite3.connect(db)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    if fetch == 1:
+        results = cursor.fetchone()
+    if fetch == 2:
+        results = cursor.fetchall()
+    connection.close()
+    return results
 
 @app.route('/')
 def home():
+    #do_query("SELECT  ", 1)
     return render_template("home.html", title = "cool tab")
 
 # tells flask what port to run on
