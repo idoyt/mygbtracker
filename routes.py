fc3c15f9549df79464449333153ab2269e11a96d
kayven <18300@burnside.school.nz>
2021-05-26 14:23:46 +1200

    editing the flask stuff and getting ready to do modals

--- routes.py
+++ routes.py
@@ -17,11 +17,35 @@ def do_query(query, fetch):
     return results
 
 @app.route('/')
-def home():
+def live():
     results = do_query("SELECT Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.status_id WHERE Status.id=1;", 2)
     number = do_query("SELECT Thread.id FROM Thread WHERE Thread.status_id = 1", 1)
     return render_template("home.html", results = results, number = number)
 
+@app.route('/gb')
+def upcoming():
+    results = do_query("SELECT Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.status_id WHERE Status.id=2;", 2)
+    number = do_query("SELECT Thread.id FROM Thread WHERE Thread.status_id = 1", 1)
+    return render_template("home.html", results = results, number = number)
+
+@app.route('/ic')
+def interestcheck():
+    results = do_query("SELECT Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.status_id WHERE Status.id=3;", 2)
+    number = do_query("SELECT Thread.id FROM Thread WHERE Thread.status_id = 1", 1)
+    return render_template("home.html", results = results, number = number)
+
+@app.route('/completed')
+def completed():
+    results = do_query("SELECT Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.status_id WHERE Status.id=4;", 2)
+    number = do_query("SELECT Thread.id FROM Thread WHERE Thread.status_id = 1", 1)
+    return render_template("home.html", results = results, number = number)
+
+@app.route('/<int:id>')
+def modal(id):
+    thread = do_query("SELECT Thread.name FROM Thread WHERE id=?;",(id,), 1)
+    return render_template("modal.html", thread = id)
+
+    return render_template("home.html", results = results, )
 # tells flask what port to run on
 if __name__ == "__main__":
     app.run(debug=True, port=1111)
