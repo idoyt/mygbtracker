8b690245a6a6b00f9bc04b2c21eae597b26aad4d
kayven <18300@burnside.school.nz>
2021-06-10 12:45:20 +1200

    modal is working

--- routes.py
+++ routes.py
@@ -6,39 +6,21 @@ db = "mygbdatabase.db"
 #index_page_query = (f"SELECT Thread.id, Photo.photo_link, Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.status_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Status.id{status_id} ORDER BY Thread.start_date DESC;")
 
 
-def do_query(query, fetch):
+def do_query(query, data = None, fetchall = False):
     connection = sqlite3.connect(db)
     cursor = connection.cursor()
-    cursor.execute(query)
-    if fetch == 1:
-        results = cursor.fetchone()
-    if fetch == 2:
-        results = cursor.fetchall()
+    if data is None:
+        cursor.execute(query)
+    else:
+        cursor.execute(query, data)
+    results = cursor.fetchall() if fetchall else cursor.fetchone()
     connection.close()
     return results
 
 @app.route('/')
 def live():
-    results = do_query("SELECT Thread.id, Photo.photo_link, Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.status_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Status.id=1 ORDER BY Thread.start_date DESC;", 2)
-    number = do_query("SELECT Thread.id FROM Thread WHERE Thread.status_id = 1", 1)
-    return render_template("index.html", results = results, number = number)
-
-@app.route('/gb')
-def upcoming():
-    results = do_query("SELECT Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.status_id WHERE Status.id=2;", 2)
-    number = do_query("SELECT Thread.id FROM Thread WHERE Thread.status_id = 1", 1)
-    return render_template("index.html", results = results, number = number)
-
-@app.route('/ic')
-def interestcheck():
-    results = do_query("SELECT Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.status_id WHERE Status.id=3;", 2)
-    number = do_query("SELECT Thread.id FROM Thread WHERE Thread.status_id = 1", 1)
-    return render_template("index.html", results = results, number = number)
-
-@app.route('/completed')
-def completed():
-    results = do_query("SELECT Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.status_id WHERE Status.id=4;", 2)
-    number = do_query("SELECT Thread.id FROM Thread WHERE Thread.status_id = 1", 1)
+    results = do_query("SELECT Thread.id, Photo.photo_link, Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.status_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Status.id=1 ORDER BY Thread.start_date DESC;", data = None , fetchall = True)
+    number = do_query("SELECT Thread.id FROM Thread WHERE Thread.status_id = 1", data = None, fetchall = False)
     return render_template("index.html", results = results, number = number)
 
 @app.route("/ajaxfile", methods=["POST","GET"])
@@ -47,8 +29,7 @@ def ajaxfile():
     cursor = connection.cursor()
     if request.method == 'POST':
         gbid = request.form['gbid']
-        print(gbid)
-        cursor.execute("SELECT Thread.id, Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.type_id WHERE Thread.id=?; ",(id,))
+        cursor.execute("SELECT Thread.id, Photo.photo_link, Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.type_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Thread.id=?; ",(gbid,))
         modal = cursor.fetchall()
     return jsonify({'htmlresponse': render_template('modal.html', modal = modal)})
 
