950c356023ea37ce5bb8de569a953aa69485605f
idoyt <18300@burnside.school.nz>
2021-07-31 22:10:52 +1200

    just making my website worky and looky nicey again

--- routes.py
+++ routes.py
@@ -17,36 +17,35 @@ def do_query(query, data = None, fetchall = False):
 
 @app.route('/')
 def index():
-    results = do_query("SELECT Thread.id, Photo.photo_link, Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.status_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Status.id=1 ORDER BY Thread.start_date DESC;", data = None , fetchall = True)
-    number = do_query("SELECT Thread.id FROM Thread WHERE Thread.status_id = 1", data = None, fetchall = False)
-    return render_template("index.html", results = results, number = number)
+    #results = do_query("SELECT Thread.id, Photo.photo_link, Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.status_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Status.id=1 ORDER BY Thread.start_date DESC;", data = None , fetchall = True)
+    return render_template("index.html") #, results = results, number = number)
 
-@app.route('/upcoming')
+@app.route('/groupbuy')
 def upcoming():
-    results = do_query("SELECT Thread.id, Photo.photo_link, Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.status_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Status.id=2 ORDER BY Thread.start_date DESC;", data = None , fetchall = True)
-    number = do_query("SELECT Thread.id FROM Thread WHERE Thread.status_id = 2", data = None, fetchall = False)
-    return render_template("index.html", results = results, number = number)
+    results = do_query("SELECT Thread.id, Photo.link, Thread.thread_name, Status.status_name, Thread.start_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Status.id=2 GROUP BY Thread.id ORDER BY Thread.start_date DESC;", data = None , fetchall = True)
+    return render_template("threads.html", results = results)
 
 @app.route('/interestcheck')
 def interestcheck():
-    results = do_query("SELECT Thread.id, Photo.photo_link, Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.status_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Status.id=3 ORDER BY Thread.start_date DESC;", data = None , fetchall = True)
-    number = do_query("SELECT Thread.id FROM Thread WHERE Thread.status_id = 3", data = None, fetchall = False)
-    return render_template("index.html", results = results, number = number)
+    results = do_query("SELECT Thread.id, Photo.link, Thread.thread_name, Status.status_name, Thread.start_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Status.id=3 GROUP BY Thread.id ORDER BY Thread.start_date DESC;", data = None , fetchall = True)
+    return render_template("threads.html", results = results)
 
-@app.route('/completed')
-def completed():
-    results = do_query("SELECT Thread.id, Photo.photo_link, Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.status_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Status.id=4 ORDER BY Thread.start_date DESC;", data = None , fetchall = True)
-    number = do_query("SELECT Thread.id FROM Thread WHERE Thread.status_id = 4", data = None, fetchall = False)
-    return render_template("index.html", results = results, number = number)
+@app.route('/archive')
+def archive():
+    results = do_query("SELECT Thread.id, Photo.link, Thread.thread_name, Status.status_name, Thread.start_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Status.id=4 GROUP BY Thread.id ORDER BY Thread.start_date DESC;", data = None , fetchall = True)
+    return render_template("threads.html", results = results)
+
+@app.route("/thread/<int:id>")
+def thread(id):
+    modal = do_query("SELECT Thread.id, Photo.link, Thread.thread_name, Status.status_name, Thread.start_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Thread.id=?; ",(id,), fetchall = True)
+    return render_template("modal.html", modal = modal)
 
 @app.route("/ajaxfile", methods=["POST","GET"])
 def ajaxfile():
     if request.method == 'POST':
         gbid = request.form['gbid']
-        modal = do_query("SELECT Thread.id, Photo.photo_link, Thread.thread_name, Status.status_name, Type.type_name, Thread.price, Thread.start_date, Thread.end_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Type ON Type.id = Thread.type_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Thread.id=?; ",(gbid,), fetchall = True)
+        modal = do_query("SELECT Thread.id, Photo.link, Thread.thread_name, Status.status_name, Thread.start_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Thread.id=?; ",(gbid,), fetchall = True)
     return jsonify({'htmlresponse': render_template('modal.html', modal = modal)})
-
-
 
 # tells flask what port to run on
 if __name__ == "__main__":
