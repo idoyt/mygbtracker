4e2317e19102f0172fff050ad47c0227b3bf48c4
idoyt <18300@burnside.school.nz>
2021-08-03 10:25:21 +1200

    search and 404

--- routes.py
+++ routes.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, flash
 import sqlite3
 
 app = Flask(__name__)
 db = "mygbdatabase.db"
+app.secret_key = "and"
+
 
 def do_query(query, data = None, fetchall = False):
     connection = sqlite3.connect(db)
@@ -41,6 +43,31 @@ def ajaxfile():
         gbid = request.form['gbid']
         results = do_query("SELECT Thread.id, Photo.link, Thread.thread_name, Status.status_name, Thread.start_date FROM Thread JOIN status ON Status.id = Thread.status_id JOIN Photo ON Thread.id = Photo.thread_id WHERE Thread.id=?; ",(gbid,), fetchall = True)
     return jsonify({'htmlresponse': render_template('modal.html', results = results)})
+
+@app.route ("/search", methods=["POST", "GET"])
+def search():
+    #search bar, allows user to search for a specific herb and redirects them to the specific page for that herb.
+    if request.method == "POST":
+        print (request.form.get("filter"))
+        results = do_query("SELECT * FROM Thread WHERE Thread.thread_name LIKE '%' || ? || '%' ORDER BY Thread.thread_name;", (request.form.get("filter"),), fetchall = True)
+        if results == None:
+            return redirect ("/error")
+        else:
+            return redirect (f"/thread/{(results[0])[0]}")
+
+@app.route ("/error")
+def error():
+    #error page, for when user input returns no results.
+    return render_template("error.html")
+
+@app.route ("/searchresults")
+def searchresults():
+    return render_template("searchresults.html", title = "Search Results")
+
+@app.errorhandler(404)
+def error404(error):
+    # note that we set the 404 status explicitly
+    return render_template('404.html')
 
 # tells flask what port to run on
 if __name__ == "__main__":
