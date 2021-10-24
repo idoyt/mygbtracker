b79cadebbbe648da6817e9184faae3fd04b26213
kayven <18300@burnside.school.nz>
2021-10-24 23:29:52 +1300

    removed capitalization where un-needed

--- static/js/areyousure.js
+++ static/js/areyousure.js
@@ -1,6 +1,6 @@
 // popup check if user wants to leave
 $(function() {
     $('.confirm').click(function() {
-        return window.confirm("Redirecting to New Page Are you sure you want to leave the page?");
+        return window.confirm("Redirecting to new page. Are you sure you want to leave the page?");
     });
 });
